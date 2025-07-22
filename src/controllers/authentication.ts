b1cb9fcf7470e0d617fac175485654a8a0af0f4f
dare-tunmise <daretunmise@yahoo.com>
2025-07-22 01:19:49 +0100

    revamped login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,34 @@ import express from 'express';
 import { getUserByEmail, createUser } from '../db/users';
 import { authentication, random } from '../helpers';
 
+export const login = async (request: express.Request, response: express.Response) => {
+    try {
+        const { email, password } = request.body;
+        if(!email || !password) {
+            return response.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if(!user) {
+            return response.sendStatus(400);
+        }
+        const expectedHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password !== expectedHash ){
+            return response.sendStatus(403)
+        }
+
+        const salt  = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();
+        response.cookie('ANTONIO-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/'});
+        return response.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return response.sendStatus(400)
+    }
+}
+
 export const register = async(request: express.Request, response: express.Response) => {
     try {
         const {email, password, username } = request.body;
@@ -29,7 +57,4 @@ export const register = async(request: express.Request, response: express.Respon
         console.log(error);
         return response.sendStatus(400);
     }
-}
-
-export const login = async (request: express.Request, response: express.Response) => {
 }
